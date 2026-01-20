daabcce5cdfbf85f9b86cac53ebaaf6ddd892c8e
amelbnsm <la237329@student.helha.be>
2026-01-20 23:10:27 +0100

    ajout fantôme déco

--- src/features/auth/layouts/LoginLayout.tsx
+++ src/features/auth/layouts/LoginLayout.tsx
@@ -1,10 +1,15 @@
 import LoginForm from '../components/LoginForm.js'
 import '../auth.css'
+import ghost from '../../../assets/ghost.png'
 
 // layout de la page de connexion
 export default function LoginLayout() {
   return (
     <div className="auth-page">
+
+        {/* élément décoratif (fantôme) */}
+        <img src={ghost} className="auth-ghost" alt="f" />
+
       <div className="auth-card">
         <h1>Connexion</h1>
 
--- src/features/auth/layouts/RegisterLayout.tsx
+++ src/features/auth/layouts/RegisterLayout.tsx
@@ -1,10 +1,14 @@
 import RegisterForm from '../components/RegisterForm.js'
 import '../auth.css'
+import ghost from '../../../assets/ghost.png'
 
 // layout pour la page d'inscription
 export default function RegisterLayout() {
   return (
     <div className="auth-page">
+
+                {/* élément décoratif (fantôme) */}
+        <img src={ghost} className="auth-ghost" alt="f" />
       <div className="auth-card">
         <h1>Inscription</h1>
 
