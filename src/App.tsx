cd63fda521c99c2e8171dc7564692ee427a8a17c
amelbnsm <la237329@student.helha.be>
2026-01-20 21:41:45 +0100

    modif de app.tsx pour le routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import { NotesLayout } from './features/notes/layouts/NotesLayout.js';
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom'
 
+// composant racine de l'app
+// gère le routing
 function App() {
-  return <NotesLayout />;
+  return (
+    <Routes>
+
+      {/*
+        route principale de l'app
+        /app correspond à l'interface de gestion de notes
+      */}
+      <Route path="/app" element={<NotesLayout />} />
+
+      {/* 
+        route racine (/)
+        on redrige automatiquement vers /app
+      */}
+      <Route path="/" element={<Navigate to="/app" replace />}/>
+
+        {/*
+        Si l'utilisateur tape une URL inconnue, on le redirige vers /app.
+      */}
+      <Route path="*" element={<Navigate to="/app" replace />} />
+    </Routes>
+  )
 }
 
 export default App;
