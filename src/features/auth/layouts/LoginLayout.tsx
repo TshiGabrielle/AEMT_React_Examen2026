cfbd3c048ca6075d9128f27e29b6b8fc228ed950
amelbnsm <la237329@student.helha.be>
2026-01-20 21:45:10 +0100

    layouts pour la page d'inscription/connexion

---
+++ src/features/auth/layouts/LoginLayout.tsx
@@ -0,0 +1,9 @@
+// layout de la page de connexion
+export default function LoginLayout() {
+    return (
+        <div style = {{ padding: '2rem' }}>
+            <h1>Login Page</h1>
+            <p>Page de connexion</p>
+        </div>
+    )
+}
---
+++ src/features/auth/layouts/RegisterLayout.tsx
@@ -0,0 +1,9 @@
+// layout pour la page d'inscription
+export default function RegisterLayout() {
+    return (
+        <div style = {{ padding: '2rem' }}>
+            <h1>Register Page</h1>
+            <p>Page d'inscription</p>
+        </div>
+    )
+}
