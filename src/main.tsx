6eb1c869875223f3a6aba92d64240950a197c9bf
amelbnsm <la237329@student.helha.be>
2026-01-20 21:37:10 +0100

    ajout du routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
+import { BrowserRouter } from 'react-router'
 
+// on enveloppe le composant App avec le BrowserRouter
+// il permet d'activer le routing de l'application
 createRoot(document.getElementById('root')!).render(
-  <App />
-);
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
