8de3ac7a4c6e0063e54f7d95975e5c831939dae7
amelbnsm <la237329@student.helha.be>
2026-01-22 10:08:23 +0100

    création du service pour l'export

---
+++ src/services/FoldersExportService.tsx
@@ -0,0 +1,13 @@
+const url = 'http://localhost:8080/api/folders';
+
+export class FoldersExportService {
+  async downloadZip(folderId: number) {
+    const res = await fetch(`${url}/${folderId}/export/zip`);
+
+    if (!res.ok) {
+      throw new Error("Erreur lors de l'export du dossier");
+    }
+
+    return await res.blob(); // on récupère le ZIP en binaire
+  }
+}
